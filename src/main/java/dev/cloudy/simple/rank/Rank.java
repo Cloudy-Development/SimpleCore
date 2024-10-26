54f9d65189f7dbcf2b35914b62ea16d121bb12d1
Emmy <lisariesey@gmail.com>
2024-10-26 18:02:52 +0200

    :(

--- src/main/java/dev/cloudy/simple/rank/Rank.java
+++ src/main/java/dev/cloudy/simple/rank/Rank.java
@@ -1,5 +1,6 @@
 package dev.cloudy.simple.rank;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.ChatColor;
@@ -11,6 +12,7 @@ import org.bukkit.ChatColor;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 public class Rank {
     private String name;
     private String prefix;
@@ -23,27 +25,4 @@ public class Rank {
     private boolean defaultRank;
     private boolean staffRank;
     private boolean donatorRank;
-
-    /**
-     * Constructor for the rank class
-     *
-     * @param name the name of the rank
-     * @param prefix the prefix of the rank
-     * @param suffix the suffix of the rank
-     * @param weight the weight of the rank
-     * @param color the color of the rank
-     * @param defaultRank if the rank is the default rank
-     * @param staffRank if the rank is a staff rank
-     * @param donatorRank if the rank is a donator rank
-     */
-    public Rank(String name, String prefix, String suffix, int weight, ChatColor color, boolean defaultRank, boolean staffRank, boolean donatorRank) {
-        this.name = name;
-        this.prefix = prefix;
-        this.suffix = suffix;
-        this.weight = weight;
-        this.color = color;
-        this.defaultRank = defaultRank;
-        this.staffRank = staffRank;
-        this.donatorRank = donatorRank;
-    }
 }
